77c01a55ba79f745643987e26811575030d08176
BosGuides <42395851+BosGuides@users.noreply.github.com>
2019-02-27 17:28:54 +0800

    Add files via upload

---
+++ 3Dviewer/hiding/component_hiding/index.js
@@ -0,0 +1,40 @@
+const op = {
+  viewport:"viewport",
+  devcode:"e10e59bf0ee97213ca7104977877bd1a",
+  viewController:false        //不加载左上角视图球
+};
+const vizbim = new BIMWINNER.Viewer(op);  // 三维主对象
+const filekey = "demo_fornature_simple";  // 模型的文件key
+let defaultCk;  // 默认的构件id
+
+// 初始化，主函数
+const init = () => {
+  vizbim.resize();  // 画布自适应
+  vizbim.showModelByDocumentId(filekey, function () {
+    restoralButton();  // 模型加载完成后恢复按钮的点击功能
+  });  // 加载模型函数
+}
+
+// 点击运行触发的函数
+const run = () =>{
+  // 如果用户点击了构件，则把选中的构件id赋值给defaultCk，否则就是默认的构件id值
+  defaultCk = vizbim.arrayConversion()
+    ? vizbim.arrayConversion()
+    : ["demo_fornature_simple_35mRxI2L175Qq8uVbXI6Nw"];
+  vizbim.hideObjs(defaultCk);  // 隐藏所选构件
+}
+
+// 点击复位触发的函数
+const reset = () =>{
+  vizbim.resetScene();  // 复位
+}
+
+// 恢复按钮的点击功能。为防止用户在未加载完成时点击按钮，造成异常
+const restoralButton = () =>{
+  $(".layui-btn").removeAttr("disabled");
+  $(".layui-btn").removeClass("layui-btn-disabled");
+  $(".layui-btn").addClass("layui-btn-primary");
+}
+
+// 主函数
+init();
